ea159f0492bff8604ac68353a854cc08055dc308
Daudi Wampamba <wampamba.daudi@gmail.com>
2022-05-18 04:23:22 +0100

    It works!

--- src/Mandelbrot.cpp
+++ src/Mandelbrot.cpp
@@ -1,79 +1,96 @@
 //
 // Created by Daudi on 16/05/22.
+// export OverrideDefaultFP64Settings=1
+// export IGC_EnableDPEmulation=1
+// dpcpp `Magick++-config --cxxflags --cppflags` src/Mandelbrot.cpp `Magick++-config --ldflags --libs` -o a.o
 //
 
 #include "Mandelbrot.h"
 
-Mandelbrot::Mandelbrot(Axes<int> cartesianBounds, Axes<double> argandBounds, int maxDepth)
-    : maxDepth(maxDepth), Cartesian(cartesianBounds), Argand(argandBounds) {}
-
-Point<double> Mandelbrot::CartesianToArgand(Point<int> cartesianPoint) const {
-    return Point<double> {
-        this->Argand.X.Start + ((double) (cartesianPoint.X - this->Cartesian.X.Start) / (this->Cartesian.X.End - this->Cartesian.X.Start)) * (this->Argand.X.End - this->Argand.X.Start),
-        this->Argand.Y.Start + ((double) (cartesianPoint.Y - this->Cartesian.Y.Start) / (this->Cartesian.Y.End - this->Cartesian.Y.Start)) * (this->Argand.Y.End - this->Argand.Y.Start)
-    };
-}
-
-double Mandelbrot::EvaluatePoint(Point<int> point) const {
-    return (double) EvaluatePoint(CartesianToArgand(point)) / maxDepth;
-}
-
-int Mandelbrot::EvaluatePoint(Point<double> point) const {
-    int depth;
-    auto zn = Point<double>{0,0};
-    for(depth = 0; depth < maxDepth && pow(zn.X,2) + pow(zn.Y,2) <= 4; ++depth) {
-        zn.X = pow(zn.X,2) - pow(zn.Y, 2) + point.X;
-        zn.Y = 2 * zn.X * zn.Y + point.Y;
+static auto exception_handler = [](sycl::exception_list e_list) {
+    for (std::exception_ptr const& e : e_list) {
+        try {
+            std::rethrow_exception(e);
+        }
+        catch (std::exception const& e) {
+            std::cout << "Failure" << std::endl;
+            std::terminate();
+        }
     }
-    return depth;
-}
-
-template <typename T>
-bool Axes<T>::hasPoint(Point<T> point) const {
-    return this->X.Start + point.X < this->X.End && this->Y.Start + point.X < this->Y.End;
-}
-
-struct BMPHeader {
-    uint16_t header;
-    uint32_t size;
-    uint16_t reserved_0;
-    uint16_t reserved_1;
 };
 
 int main(int argc, char *argv[]) {
-    auto mandelbrot = new Mandelbrot(
-        Axes<int>{
-            Dimension<int>{0,1920},
-            Dimension<int>{0,1080}
-        },
-        Axes<double>{
-            Dimension<double>{0,1920},
-            Dimension<double>{0,1080}
-        },
-        10
-    );
+    // Parameters
+    const float argandStartX = -2;
+    const float argandEndX = 0.75;
+    const float argandStartY = -0.7734375;
+    const float argandEndY = 0.7734375;
+    const unsigned int imageWidth = 15360;
+    const unsigned int imageHeight = 8640;
+    const unsigned long maxDepth = 1000;
 
-    // Create file
-    FILE * fp;
-    if (!(fp = fopen("image.bmp", "wb"))) {
-        return 3;
-    }
+    // Calculated
+    const float argandRangeX = argandEndX - argandStartX;
+    const float argandRangeY = argandEndY - argandStartY;
 
-    fwrite(, sizeof(char), 1, fp);
-    fwrite(bmp_dib_v3_header, sizeof(bmp_dib_v3_header_t), 1, fp);
+    std::cout << "Creating Sycl Job..." << std::endl;
 
-    Point<int> cursor{};
-    for (cursor.X = 0; cursor.X < 1920; cursor.X++)  {
-        for (cursor.Y = 0; cursor.Y < 1080; cursor.Y++) {
-            int color = floor(255 * mandelbrot->EvaluatePoint(cursor));
-            fwrite(&color, 1, 1, fp);
-            fwrite(&color, 1, 1, fp);
-            fwrite(&color, 1, 1, fp);
+    // Initialise device
+    sycl::default_selector d_selector;
+
+    // Create a command queue
+    sycl::queue queue(d_selector, exception_handler);
+
+    // Report on the device the queue is using
+    std::cout << "Using device: " << queue.get_device().get_info<sycl::info::device::name>() << std::endl;
+
+    sycl::range<2> num_pixels{ imageWidth, imageHeight };
+
+    sycl::buffer<unsigned int, 2> buffer(num_pixels);
+
+    queue.submit([&](sycl::handler &h) {
+        sycl::accessor buff_access(buffer, h, sycl::write_only);
+
+        h.parallel_for(num_pixels, [=](sycl::id<2> i) {
+            unsigned long x = i[0], y = i[1];
+            unsigned int depth;
+
+            std::complex<float> zn(0, 0);
+            std::complex<float> c(((float) x / imageWidth) * argandRangeX + argandStartX, ((float) y / imageHeight) * argandRangeY + argandStartY);
+
+            for (depth = 0; depth < maxDepth && abs(zn) <= 2; ++depth) {
+                zn = pow(zn, 2) + c;
+            }
+
+            buff_access[x][y] = depth;
+        });
+    });
+
+    std::cout << "Sycl kernels launched" << std::endl;
+
+    queue.wait_and_throw();
+
+    std::cout << "Sycl kernels completed" << std::endl;
+
+    // Create Image
+    std::cout << "Creating Image" << std::endl;
+
+    Magick::InitializeMagick(*argv);
+
+    sycl::host_accessor hostAccessor(buffer);
+
+    auto image = new Magick::Image(Magick::Geometry(imageWidth, imageHeight), Magick::ColorRGB(0, 0, 0, 0));
+
+    for(int i = 0; i < imageWidth; i++) {
+        for(int j = 0; j < imageHeight; j++) {
         }
     }
 
-    /* cleanup heap allocation */
-    fclose(fp);
+    image->write("/home/daudi/Code/Mandelbrot/Mandelbrot.png");
+
+    Magick::TerminateMagick();
+
+    std::cout << "Image created" << std::endl;
 
     return 0;
 }
--- src/Mandelbrot.h
+++ src/Mandelbrot.h
@@ -6,42 +6,11 @@
 #define MANDELBROT_MANDELBROT_H
 
 #include <cmath>
+#include <complex>
 #include <iostream>
 #include <CL/sycl.hpp>
-#include <libpng16/png.h>
+#include <Magick++.h>
 
-template <typename T>
-struct Dimension {
-public:
-    T Start;
-    T End;
-} ;
-
-template <typename T>
-struct Point {
-public:
-    T X;
-    T Y;
-} ;
-
-template <typename T>
-struct Axes {
-public:
-    Dimension<T> X;
-    Dimension<T> Y;
-    [[nodiscard]] bool hasPoint(Point<T> point) const;
-} ;
-
-class Mandelbrot {
-    Axes<int> Cartesian;
-    Axes<double> Argand;
-    int maxDepth;
-private:
-    [[nodiscard]] Point<double> CartesianToArgand(Point<int> cartesianPoint) const;
-    [[nodiscard]] int EvaluatePoint(Point<double> point) const;
-public:
-    Mandelbrot(Axes<int> cartesianBounds, Axes<double> argandBounds, int maxDepth);
-    [[nodiscard]] double EvaluatePoint(Point<int> point) const;
-};
+// M T
 
 #endif //MANDELBROT_MANDELBROT_H
